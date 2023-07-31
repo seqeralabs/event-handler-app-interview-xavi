4b2d5d48db671e612944f74987c0b985f3f3bf98
Xavier Arias <xavi.arias.segui@gmail.com>
2023-07-31 23:52:23 +0200

    Add missing test

--- app/src/test/groovy/io/seqera/events/utils/db/PooledDataSourceTest.groovy
+++ app/src/test/groovy/io/seqera/events/utils/db/PooledDataSourceTest.groovy
@@ -42,25 +42,47 @@ class PooledDataSourceTest {
     @Test
     void 'database connections are allocated at startup'() {
         def dataSource = pooledDataSource()
+
+        // The initial pool size of connections should have been created at startup
         verify(driverMock, times(dataSource.initialPoolSize)).connect(eq("jdbc:test:events"), any())
     }
 
     @Test
-    void 'database connections are not closed by the application code'() {
+    void 'database connections are not physically closed by the application code'() {
         def dataSource = pooledDataSource()
         dataSource.getConnection().close()
 
         for (connection in connectionMocks) {
+            // None of the connection mocks should be closed
             verify(connection, never()).close()
         }
     }
 
     @Test
+    void 'database connections are released when closed by the application code'() {
+        def dataSource = pooledDataSource()
+        List<Connection> connections = []
+
+        // Consume all connection pool
+        for (int i = 0; i < initialPoolSize; i++) {
+            connections += dataSource.getConnection()
+        }
+
+        // Closing a connection should make it available again
+        connections.last().close()
+        dataSource.getConnection()
+    }
+
+    @Test
     void 'an exception is thrown when no connections are available in the pool'() {
         def dataSource = pooledDataSource()
+
+        // Consume all connection pool
         for (int i = 0; i < initialPoolSize; i++) {
             dataSource.getConnection()
         }
+
+        // Next connection request should fail
         assertThrows(SQLException.class) {
             dataSource.getConnection()
         }
